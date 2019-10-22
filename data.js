ad75d254a837eb4935f0249bd24d83454b8c85f7
illiamw <william.luis.ferreira@usp.br>
2019-10-22 15:46:32 -0300

    update arquivos security

--- data.js
+++ data.js
@@ -22,11 +22,9 @@ const NUM_DATASET_ELEMENTS = 65000;
 const NUM_TRAIN_ELEMENTS = 55000;
 const NUM_TEST_ELEMENTS = NUM_DATASET_ELEMENTS - NUM_TRAIN_ELEMENTS;
 
-const MNIST_IMAGES_SPRITE_PATH =
-  "https://storage.googleapis.com/learnjs-data/model-builder/mnist_images.png";
-const MNIST_LABELS_PATH =
-  "https://storage.googleapis.com/learnjs-data/model-builder/mnist_labels_uint8";
-  console.log(MNIST_IMAGES_SPRITE_PATH);
+const MNIST_IMAGES_SPRITE_PATH = "./data/mnist_images.png";
+const MNIST_LABELS_PATH =  "./data/mnist_labels_uint8";
+console.log(MNIST_IMAGES_SPRITE_PATH);
 
 /**
  * A class that fetches the sprited MNIST dataset and returns shuffled batches.
